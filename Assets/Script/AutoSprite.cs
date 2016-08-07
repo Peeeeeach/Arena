ddb8b2c1d51a9f9f83ed25e6b4d456efb3ef525f
wxz97121 <wxz97121@gmail.com>
2016-08-07 16:25:14 +0800

    增加了一部分美术资源

---
+++ Assets/Script/AutoSprite.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AutoSprite : MonoBehaviour {
+    public Sprite Front,Back,Left,Right;
+    public float Least;
+    private Vector3 speed;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (gameObject.name == "PlayerSquare")
+            speed = GetComponentInParent<Rigidbody2D>().velocity;
+        else speed = GetComponent<Rigidbody2D>().velocity;
+        if (speed.magnitude>Least)
+        {
+            if (Mathf.Abs(Vector3.Angle(speed, Vector3.left)) < 45) gameObject.GetComponent<SpriteRenderer>().sprite = Left;
+            if (Mathf.Abs(Vector3.Angle(speed, Vector3.right)) < 45) gameObject.GetComponent<SpriteRenderer>().sprite = Right;
+            if (Mathf.Abs(Vector3.Angle(speed, Vector3.down)) < 45) gameObject.GetComponent<SpriteRenderer>().sprite = Front;
+            if (Mathf.Abs(Vector3.Angle(speed, Vector3.up)) < 45) gameObject.GetComponent<SpriteRenderer>().sprite = Back;
+        }
+	}
+}
---
+++ Assets/Script/FireballRotate.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FireballRotate : MonoBehaviour {
+    private Vector3 speed;
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        speed = GetComponent<Rigidbody2D>().velocity;
+        Debug.Log(Mathf.Atan2(speed.y, speed.x));
+        transform.rotation = Quaternion.Euler(0, 0, Mathf.Atan2(speed.y,speed.x)*360/2/Mathf.PI);
+/*        if (Mathf.Abs(Vector3.Angle(speed, Vector3.left)) < 45) transform.rotation = Quaternion.Euler(0,0,180);
+        if (Mathf.Abs(Vector3.Angle(speed, Vector3.right)) < 45) transform.rotation = Quaternion.Euler(0,0,0); 
+        if (Mathf.Abs(Vector3.Angle(speed, Vector3.down)) < 45) transform.rotation = Quaternion.Euler(0,0,-90);
+        if (Mathf.Abs(Vector3.Angle(speed, Vector3.up)) < 45) transform.rotation = Quaternion.Euler(0,0,90);
+*/
+    }
+}
