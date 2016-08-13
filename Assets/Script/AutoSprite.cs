19ecbd21f29b3a1d25244784e3e944ca7edc1347
CH SUN <cancersun@126.com>
2016-08-14 01:14:15 +0800

    导入音频资源以及火龙与士兵的资源

--- Assets/Script/AutoSprite.cs
+++ Assets/Script/AutoSprite.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 public class AutoSprite : MonoBehaviour {
     public Sprite Front,Back,Left,Right;
+//	public GameObject Front,Back,Left,Right;
     public float Least;
     private Vector3 speed;
 	// Use this for initialization
@@ -17,10 +18,34 @@ public class AutoSprite : MonoBehaviour {
         else speed = GetComponent<Rigidbody2D>().velocity;
         if (speed.magnitude>Least)
         {
-            if (Mathf.Abs(Vector3.Angle(speed, Vector3.left)) < 45) gameObject.GetComponent<SpriteRenderer>().sprite = Left;
-            if (Mathf.Abs(Vector3.Angle(speed, Vector3.right)) < 45) gameObject.GetComponent<SpriteRenderer>().sprite = Right;
-            if (Mathf.Abs(Vector3.Angle(speed, Vector3.down)) < 45) gameObject.GetComponent<SpriteRenderer>().sprite = Front;
-            if (Mathf.Abs(Vector3.Angle(speed, Vector3.up)) < 45) gameObject.GetComponent<SpriteRenderer>().sprite = Back;
+			if (Mathf.Abs (Vector3.Angle (speed, Vector3.left)) < 45) {
+				gameObject.GetComponent<SpriteRenderer> ().sprite = Left;
+//				Front.SetActive(false);
+//				Back.SetActive (false);
+//				Left.SetActive (true);
+//				Right.SetActive (false);
+			}
+			if (Mathf.Abs (Vector3.Angle (speed, Vector3.right)) < 45) {
+				gameObject.GetComponent<SpriteRenderer> ().sprite = Right;
+//				Front.SetActive(false);
+//				Back.SetActive (false);
+//				Left.SetActive (false);
+//				Right.SetActive (true);
+			}
+			if (Mathf.Abs (Vector3.Angle (speed, Vector3.down)) < 45) {
+				gameObject.GetComponent<SpriteRenderer> ().sprite = Front;
+//				Front.SetActive(true);
+//				Back.SetActive (false);
+//				Left.SetActive (false);
+//				Right.SetActive (false);
+			}
+			if (Mathf.Abs (Vector3.Angle (speed, Vector3.up)) < 45) {
+				gameObject.GetComponent<SpriteRenderer> ().sprite = Back;
+//				Front.SetActive(false);
+//				Back.SetActive (true);
+//				Left.SetActive (false);
+//				Right.SetActive (false);
+			}
         }
 	}
 }
